ecdbf4300c24aee0f34ab8a4472679b4d03a19ca
unknown <max.reimann@student.hpi.uni-potsdam.de>
2016-05-06 00:26:57 +1000

    fire firy-bullets from witch

--- Assets/witchController.cs
+++ Assets/witchController.cs
@@ -6,17 +6,25 @@ public class witchController : MonoBehaviour {
 	Transform myTrans;
 	Rigidbody2D myBody;
 	bool facingLeft = true;
+	private Transform firePoint;
+	public Rigidbody2D bullet;
 	// Use this for initialization
 	void Start () 
 	{
 		myTrans = this.transform;
 		myBody = GetComponent<Rigidbody2D>();
+		GameObject emitterpoint = this.transform.FindChild("emitterpoint").gameObject;
+		firePoint = emitterpoint.transform;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		Move (Input.GetAxisRaw ("Horizontal"));
+
+		if (Input.GetKeyDown("space")) {
+			FireBullet();
+		}
 
 	}
 
@@ -35,6 +43,12 @@ public class witchController : MonoBehaviour {
 				myTrans.localRotation = Quaternion.Euler (0, 0, 0);
 			}
 		}
+	}
+
+	public void FireBullet()
+	{
+		Rigidbody2D bulletClone = (Rigidbody2D) Instantiate(bullet, firePoint.position, firePoint.rotation);
+		bulletClone.velocity = transform.up * speed;
 	}
 
 }
