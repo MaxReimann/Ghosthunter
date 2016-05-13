ced26ed12f09823406db3f5f296d7169fd8b748a
unknown <max.reimann@student.hpi.uni-potsdam.de>
2016-05-13 15:05:18 +1000

    use spellstartPoint

--- Assets/Scripts/WizardController.cs
+++ Assets/Scripts/WizardController.cs
@@ -5,10 +5,12 @@ public class WizardController : MonoBehaviour {
 
 	public float speed = 10;
 	private Rigidbody2D myBody;
+	private Transform spellStartPoint; 
 
 	// Use this for initialization
 	void Start (){
 		myBody = GetComponent<Rigidbody2D>();
+		spellStartPoint = transform.Find("spellStartPoint");
 	}
 	
 	// Update is called once per frame
@@ -30,9 +32,7 @@ public class WizardController : MonoBehaviour {
 	
 	public void createSpell(){
 		Vector3 newPos = new Vector3 ();
-		newPos.x = transform.position.x+0.9f;
-		newPos.y = transform.position.y-0.4f;
-		newPos.z = transform.position.z;
+		newPos = spellStartPoint.position;
 
 		GameObject spell = Instantiate(Resources.Load("Spell"), newPos, Quaternion.identity) as GameObject;
 		Rigidbody2D rigidBody = spell.GetComponent<Rigidbody2D>();
