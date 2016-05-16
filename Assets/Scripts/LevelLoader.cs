b7be3f38dfec3932038ff811e08f87cf3c096d95
AlexanderWolz <alexander.wolz@gmx.de>
2016-05-16 14:11:24 +1000

    added simple menu with a button

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+
+	public void loadLevel1(){
+		Debug.Log ("loading first level...");
+		Application.LoadLevel("Level1");
+	}
+
+}
