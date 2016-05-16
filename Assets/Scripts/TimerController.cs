51056131076961432be97c21d94eb2537e6338f7
AlexanderWolz <alexander.wolz@gmx.de>
2016-05-16 13:48:34 +1000

    added score and timer label (including scripts and timeout function)

---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour {
+
+	public float timer = 30;
+	private Text txt;
+
+	private GameManager gameManager;
+
+	// Use this for initialization
+	void Start () {
+		txt = gameObject.GetComponent<Text>(); 
+		gameManager = GameManager.instance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		txt.text = "Time: "+(int)timer;
+		if (timer < 1) {
+			//game over
+			gameManager.gameOver();
+		}
+	}
+}
