56bc56a7d544c87b0a5062fc77d4b3b2b6eaa1a2
AlexanderWolz <alexander.wolz@gmx.de>
2016-05-21 20:56:24 +1000

    added level 3 (moving ceiling)

---
+++ Assets/MovingPlatformController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatformController : MonoBehaviour {
+
+	private GameObject child;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = gameObject.transform.position;
+		pos.y = pos.y - 0.001f;
+		gameObject.transform.position = pos;
+	}
+}
