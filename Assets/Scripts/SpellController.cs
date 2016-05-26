ec49acfc527abac93a91bc145330dcf45c727c11
unknown <max.reimann@student.hpi.uni-potsdam.de>
2016-05-26 12:05:22 +1000

    Permanent spell

--- Assets/Scripts/SpellController.cs
+++ Assets/Scripts/SpellController.cs
@@ -3,17 +3,59 @@ using System.Collections;
 
 public class SpellController : MonoBehaviour {
 
+	public enum SpellType {
+		Normal,
+		Permanent
+	};
+
+	private bool isHalted = false;
+	private static float haltTime = 5.0f;
+
+	public SpellType spellType;
+
 	// Use this for initialization
 	void Start () {
 		transform.GetChild(0).GetComponent<Renderer>().sortingLayerName="Spells";
+		this.spellType = SpellType.Permanent;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	}
 
+	void Destroy()
+	{
+		Destroy(this.gameObject);
+	}
+
+	void HaltSpell() {
+		if (isHalted)
+			return;
+
+		Rigidbody2D rigidBody = this.GetComponent<Rigidbody2D>();
+		rigidBody.velocity = new Vector2 (0, 0);
+
+		Invoke ("Destroy", haltTime);
+		
+	}
+
 	// called by child script
 	public void collision(Collision2D coll) {
-		Destroy(this.gameObject);
+
+		if (coll.gameObject.CompareTag ("Ghost")) {
+			Destroy();
+			return;
+		}
+		
+		switch (spellType) {
+			case SpellType.Normal:
+				Destroy();
+				break;
+			case SpellType.Permanent:
+				HaltSpell();
+				break;
+		}
+
+
 	}
 }
