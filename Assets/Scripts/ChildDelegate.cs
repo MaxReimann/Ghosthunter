fa083b6a0c2fbc1345cc378e17684c6454c09788
unknown <max.reimann@student.hpi.uni-potsdam.de>
2016-06-14 22:41:54 +1000

    added pretty cool animation

---
+++ Assets/Scripts/ChildDelegate.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IChildDelegator
+{
+	// interface members
+	void childEnterCollision2D (Collision2D coll);
+	void childEnterTrigger2D (Collider2D coll);
+}
+
+//use this class to delegate events of children to parents
+public class ChildDelegate : MonoBehaviour {
+
+	[Tooltip("This controller must implement interface IChildDelegator!")]
+	public MonoBehaviour parentController;
+
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnCollisionEnter2D(Collision2D coll){
+		//will through errors, if parentcontroller doesnt implement methods.
+		(parentController as IChildDelegator).childEnterCollision2D (coll);
+	}
+	
+	void OnEnterTrigger2D(Collider2D coll){
+		(parentController as IChildDelegator).childEnterTrigger2D (coll);
+	}
+	
+	
+	
+}
---
+++ Assets/Scripts/KinematicRockController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KinematicRockController : MonoBehaviour, IChildDelegator {
+
+	public GameObject collisionVase;
+	public GameObject collisionVaseUpperHalf;
+
+	private bool collisionHappened = false;
+	private Animator animator;
+	
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+
+
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.Equals (collisionVase) && !collisionHappened) {
+			animator.SetTrigger ("hit_vase");
+			GetComponent<Rigidbody2D> ().isKinematic = true; //stop body
+			collisionHappened  = true;
+			Invoke("doDestroy",1.05f);
+			Destroy(collisionVase);
+			Rigidbody2D flyingVaseRigidBody = collisionVaseUpperHalf.GetComponent<Rigidbody2D>();
+			flyingVaseRigidBody.isKinematic = false;
+			flyingVaseRigidBody.WakeUp();
+			flyingVaseRigidBody.AddForceAtPosition(	new Vector2(-80.5f, 400.0f), new Vector2(0.1f,-0.01f));
+			collisionVaseUpperHalf.GetComponent<SpriteRenderer>().enabled = true;
+			Invoke("changeUpperVaseLayer", 0.6f);
+
+			
+		}
+	}
+
+	void changeUpperVaseLayer() {
+		//make upper half collidable with other kinematic objects
+		//do this after some delay after rock collision, to avoid colliding and stopping rock
+		collisionVaseUpperHalf.layer = LayerMask.NameToLayer ("KinematicCollider");
+		collisionVaseUpperHalf.GetComponent<PolygonCollider2D> ().enabled = true;
+	}
+
+	void doDestroy(){
+		Destroy (this.gameObject);
+	}
+
+	//interface methods
+	public void childEnterCollision2D (Collision2D coll){
+		if (coll.gameObject.CompareTag("Spell"))
+			GetComponent<Rigidbody2D> ().isKinematic = false; //activate rigibody rolling
+	}
+
+
+	public void childEnterTrigger2D (Collider2D coll){
+	} 
+	
+
+}
